56365f824b4b67f83da55fb5afe05fdc7e54df5d
Laura Levy <llevy510@gmail.com>
2020-10-06 17:44:04 -0400

    created total var which is calculated w amount, roi, and yrs

--- script.py
+++ script.py
@@ -5,3 +5,5 @@ print("hello, world")
 amount = float(input('Principal amount ?'))
 roi = float(input('Rate of Interest ?'))
 yrs = int(imput('Duration (no. of years) ?'))
+
+total = (amount * pow(1 +(roi/100), yrs))
