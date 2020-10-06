38348a3ebb5595606aa1f7092e23838177257c8f
Laura Levy <llevy510@gmail.com>
2020-10-06 17:43:10 -0400

    added a comment I should have added like 3 commits ago w explanation of waht the code below it does

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 print("hello, world")
 
+
+/interest calculator code
 amount = float(input('Principal amount ?'))
 roi = float(input('Rate of Interest ?'))
 yrs = int(imput('Duration (no. of years) ?'))
