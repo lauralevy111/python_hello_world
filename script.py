b96dfb257f302769800f6e36bd94d40434153576
Laura Levy <llevy510@gmail.com>
2020-10-06 17:44:48 -0400

    changed definition of interest

--- script.py
+++ script.py
@@ -7,4 +7,4 @@ roi = float(input('Rate of Interest ?'))
 yrs = int(imput('Duration (no. of years) ?'))
 
 total = (amount * pow(1 +(roi/100), yrs))
-interest = amount - total
+interest = total - amount
