1cad02f936fe08b645ab2ebdad6cd689ce94af21
Laura Levy <llevy510@gmail.com>
2020-10-21 08:52:33 -0400

    initialized & declared bool active

--- script.py
+++ script.py
@@ -6,6 +6,8 @@ amount = float(input('Principal amount ?'))
 roi = float(input('Rate of Interest ?'))
 yrs = int(input('Duration (no. of years) ?'))
 
+active = bool(True)
+
 total = (amount * pow(1 +(roi/100), yrs))
 interest = total - amount
 
